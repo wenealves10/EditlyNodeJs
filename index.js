68b3ec502a48d6cccf55be7a31c458c40c9eb8f7
Wene Alves de Oliveira <weneplay5@gmail.com>
2022-01-18 09:15:13 -0300

    first commit

--- index.js
+++ index.js
@@ -3,24 +3,38 @@ const editly = require('editly');
 
 
 const editSpec = {
-    outPath: './subtitle.mp4',
-    defaults: {
-      layerType: { 'fill-color': { color: '#00aa00' } }
-    },
-    clips: [
-      { duration: 10, layers: [
-        { type: 'rainbow-colors' },
-        { type: 'subtitle', text: 'Aaaaiiiii que bolinho gostoso...' },
-        { type: 'title', position: 'top', text: 'Subtitles', },
-      ] },
-      { duration: 20, layers: [
-        { type: 'fill-color' },
-        { type: 'title', position: { x: 0, y: 1, originY: 'bottom' }, text: 'Custom position', zoomDirection: null },
-      ] },
-    ],
-  }
+  outPath: './musica.mp4',
+  allowRemoteRequests: true,
+  keepSourceAudio: true,
+  defaults: {
+    layerType: {
+      'fill-color': {
+        color: '#00aa00'
+      }
+    }
+  },
+  clips: [{
+    duration: 30,
+    layers: [{
+        type: 'image',
+        path: 'https://i.ytimg.com/vi/LQ9XbPAowCQ/maxresdefault.jpg',
+        resizeMode: 'contain-blur'
+      },
+      {
+        type: 'audio',
+        path: 'https://s3.eu-central-1.wasabisys.com/storage-lindasmensagens/audio/video.mp3',
+        cutFrom: 27
+      },
+      {
+        type: 'title',
+        position: 'top',
+        text: 'MORENA',
+      },
+    ]
+  }],
+}
 async function main() {
-    await editly(editSpec)
-  .catch(console.error);
+  await editly(editSpec)
+    .catch(console.error);
 }
 main()
